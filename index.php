7773369c9c8edbc83c89c75dcaa2ba897c0db57d
Patrick Dawkins <pjcdawkins@gmail.com>
2015-07-05 14:04:29 +0100

    Remove Bootstrap JS

--- index.php
+++ index.php
@@ -61,7 +61,6 @@ if (!empty($sound_files)) {
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <script src="node_modules/soundmanager2/script/soundmanager2-jsmin.js"></script>
 <script src="js/main.js"></script>
 </body>
